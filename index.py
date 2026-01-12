bcf6a2069487e386f2ca0d471e4cb45a34013384
pratikdevelop <pratikraut88895@gmail.com>
2026-01-12 23:01:39 +0530

    code update

--- index.py
+++ index.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, roc_auc_score
 # ───────────────────────────────────────────────
 # Page config (looks nicer)
 # ───────────────────────────────────────────────
